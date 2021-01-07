e2eed5cce0cfa0c9e82ec539dfb56f968315386e
Nibin Benny <nibinbenny47@gmail.com>
2021-01-07 20:13:47 +0530

    edit and delete are done

--- State.aspx.cs
+++ State.aspx.cs
@@ -11,6 +11,7 @@ using System.Data;
 public partial class District : System.Web.UI.Page
 {
     SqlConnection con = new SqlConnection("Data Source=.;Initial Catalog=db_myproject;Integrated Security=True");
+    public static int id, status = 0;
     protected void Page_Load(object sender, EventArgs e)
     {
         con.Open();
@@ -43,6 +44,62 @@ public partial class District : System.Web.UI.Page
         adp.Fill(dt);
         grdDetails.DataSource = dt;
         grdDetails.DataBind();
+
+    }
+    protected void grdDetails_RowCommand(object sender, GridViewCommandEventArgs e)
+    {
+
+        id = Convert.ToInt32(e.CommandArgument.ToString());
+
+
+        if (e.CommandName == "del")
+        {
+
+            SqlCommand cmd = new SqlCommand("deleteState", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.Add("@stateid", SqlDbType.VarChar).Value = id;
+            cmd.Parameters.Add("@statename", SqlDbType.VarChar).Value = txtName.Text;
+            cmd.ExecuteNonQuery();
+            //string delQry = "delete from tbl_state where state_id='" + id + "'";
+            //SqlCommand cmd = new SqlCommand(delQry, con);
+            //cmd.ExecuteNonQuery();
+
+
+
+            fillDetails();
+
+        }
+
+        if (e.CommandName == "ed")
+        {
+
+            string sel = "select * from tbl_state where state_id='" + id + "'";
+            DataTable dt = new DataTable();
+            SqlDataAdapter adp = new SqlDataAdapter(sel, con);
+            adp.Fill(dt);
+            if (dt.Rows.Count > 0)
+            {
+
+
+                txtName.Text = dt.Rows[0]["state_name"].ToString();
+
+                status = 1;
+
+
+
+
+            }
+
+
+
+
+        }
+
+
+
+
+
+
 
     }
 }
